a0d53949b7330e1215d8b7d11a3de1dfdf88bcce
Zeinab Farhat <zeinab.farhat@mmpww.com>
2022-09-19 16:17:10 +0300

    Minor changes are done

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Users\UserController;
 use App\Http\Controllers\Api\Permissions\PermissionController;
-use  App\Http\Controllers\Roles\RoleController;
-use App\Http\Controllers\API\Auth\RegisterController;
-use App\Http\Controllers\Auth\AuthController;
+use  App\Http\Controllers\Api\Roles\RoleController;
+use App\Http\Controllers\Api\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,18 +18,17 @@ use App\Http\Controllers\Auth\AuthController;
 */
 
 Route::prefix('auth')->group(function () {
-    Route::post('login',  [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
- Route::get('roles/get_all_roles', [RoleController::class, 'get_all_roles']);
- Route::get('permissions/get_all_permissions', [PermissionController::class, 'get_all_permissions']);
+Route::get('roles/get_all_roles', [RoleController::class, 'get_all_roles']);
+Route::get('permissions/get_all_permissions', [PermissionController::class, 'get_all_permissions']);
 Route::middleware('auth:sanctum')->group(function () {
- Route::resource('users', UserController::class);
- Route::resource('/permissions', PermissionController::class);
- Route::resource('/roles', RoleController::class);
- Route::prefix('role')->group(function () {
-              Route::resource('permission', 'Roles\RolePermissionController');
-          });
+    Route::resource('users', UserController::class);
+    Route::resource('/permissions', PermissionController::class);
+    Route::resource('/roles', RoleController::class);
+    Route::prefix('role')->group(function () {
+        Route::resource('permission', 'Roles\RolePermissionController');
+    });
 });
 
